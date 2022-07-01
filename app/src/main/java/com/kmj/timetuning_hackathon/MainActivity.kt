efc363334fd959744198cd6e929160aed99594c8
Koominji <mjk2715@naver.com>
2022-07-01 22:48:49 +0900

    First commit

---
+++ app/src/main/java/com/kmj/timetuning_hackathon/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.kmj.timetuning_hackathon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    // ========= 미림님 페이지 ! ===========
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
